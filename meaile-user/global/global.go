abb1235943813d2e6cf4e4809106dd12d9026aa8
Shenchangxin <2863727114@qq.com>
2024-09-30 17:17:52 +0800

    集成minio并添加工具类

--- meaile-user/global/global.go
+++ meaile-user/global/global.go
@@ -3,9 +3,11 @@ package global
 import (
 	"gorm.io/gorm"
 	"meaile-server/meaile-user/config"
+	"meaile-server/meaile-user/utils"
 )
 
 var (
 	DB           *gorm.DB
 	ServerConfig *config.ServerConfig = &config.ServerConfig{}
+	MinioClient  *utils.MinioClient
 )
